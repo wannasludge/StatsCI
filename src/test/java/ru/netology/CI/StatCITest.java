41975c89ce5c0880bf7cbc388a016bc508e0b63e
Alexander mokhov <wannasludge@yandex.ru>
2021-10-04 19:31:56 +0300

    Integration and learning 'check', get 100% coverage

--- src/test/java/ru/netology/CI/StatCITest.java
+++ src/test/java/ru/netology/CI/StatCITest.java
@@ -9,12 +9,17 @@ class StatisticsServiceTest {
     @Test
     void findMax() {
         StatCI service = new StatCI();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
-
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxFirstZero(){
+        StatCI service = new StatCI();
+        long[] incomesInBillions = {0, 14, 8, 9, 0, 3, 8, 6, 5, 11, 1};
+        long expected = 14;
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
 }
