bcc81c8b4afddb20a697b8c9958ad7bf83b1006b
Alexander mokhov <wannasludge@yandex.ru>
2021-10-04 17:26:10 +0300

    copy progect

---
+++ src/main/java/ru/netology/CI/StatCI.java
@@ -0,0 +1,16 @@
+package ru.netology.CI;
+
+public class StatCI {
+/**
+ * @param incomes - array of incomes
+   * @return - index of first max value
+   */
+    public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+        return current_max;
+    }
+}
